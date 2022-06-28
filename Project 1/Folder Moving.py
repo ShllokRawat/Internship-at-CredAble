bb44cbab7c6d5fa16258407b591178f0d7216212
ShllokRawat <87135748+ShllokRawat@users.noreply.github.com>
2022-06-28 12:24:48 +0530

    Delete Project 1 directory

--- Project 1/Folder Moving.py
+++
@@ -1,4 +0,0 @@
-import os, shutil
-os.chdir('C:\\')
-os.system('mkdir Extraction')
-shutil.move('C:\\Users\\shllo\\Desktop\\Internship\\Data\\Extracted Files\\427_RMWH1649332131561', 'C:\\Users\\shllo\\Desktop\\Internship\\Data\\Extraction')
